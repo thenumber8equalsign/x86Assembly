eaa7ccbb5d66374ffb5ad03cbd92890a7995ccf5
thenumber8equalsign <168995087+thenumber8equalsign@users.noreply.github.com>
2026-06-19 14:31:18 -0400

    Enter Commit Message

---
+++ src/lib/strlen.s
@@ -0,0 +1,13 @@
+BITS 64
+DEFAULT REL
+
+section .text
+    global strlen
+strlen:
+    xor     eax, eax
+    .L3:
+        cmp     byte [rdi+rax], 0
+        je      .done
+        inc     rax
+        jmp     .L3
+    .done:
