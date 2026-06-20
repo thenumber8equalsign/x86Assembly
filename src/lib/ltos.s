1fd34417b883b94b0673d64666fcef05aea595ac
thenumber8equalsign <168995087+thenumber8equalsign@users.noreply.github.com>
2026-06-20 19:17:50 -0400

    Add the signed version for ultos

--- src/lib/ltos.s
+++ src/lib/ltos.s
@@ -12,7 +12,7 @@ section .text
 ; char* ultos(uint64_t num, uint64_t base)
 ultos:
     ; find the number of digits in the number
-    call uldigits ; this preserves rdi and rsi
+    call    uldigits ; this preserves rdi and rsi
     inc     rax ; add 1 for null terminator
     push    rdi
     push    rsi
@@ -20,8 +20,8 @@ ultos:
     ; mmap(NULL, len, 3, 34, 0, 0)
     mov     rsi, rax
     mov     rdi, 0
-    mov     rdx, 3
-    mov     r10, 34
+    mov     rdx, 3 ; PROT_READ|PROT_WRITE
+    mov     r10, 34 ; MAP_ANON|MAP_PRIVATE
     mov     r8, 0
     mov     r9, 0
     mov     rax, 9
@@ -30,9 +30,13 @@ ultos:
 
     pop     r8  ; length in r8
     dec     r8  ; this is now the index of the null terminator
-    mov     byte [rcx+r8], 0
     pop     rsi
     pop     rax ; put the number in rax for the div instruction
+
+    cmp     rcx, -1
+    je      .ultos_map_fail
+
+    mov     byte [rcx+r8], 0
     lea     r11, [digits] ; load the address of digits into r11 so that we can get some apple pie (get it haha funny)
     mov     r9, r8
 .L3:
@@ -47,4 +51,60 @@ ultos:
     jnz     .L3
 .ultos_done:
     mov     rax, rcx
+    ret
+.ultos_map_fail:
+    xor     eax, eax
+    ret
+
+; char* ltos(uint64_t num, uint64_t base)
+ltos:
+    test    rdi, rdi
+    jns     ultos   ; if rdi>=0, ultos(num, base)
+
+    ; This section is nearly identical to ultos, with the sign
+    ; find the number of digits in the number
+    neg     rdi ; negate the number first
+    call    uldigits ; this preserves rdi and rsi
+    add     rax, 2 ; add 1 for the null terminator, add another 1 for the -ve sign
+    push    rdi
+    push    rsi
+    push    rax
+    ; mmap(NULL, len, 3, 34, 0, 0)
+    mov     rsi, rax
+    mov     rdi, 0
+    mov     rdx, 3 ; PROT_READ|PROT_WRITE
+    mov     r10, 34 ; MAP_ANON|MAP_PRIVATE
+    mov     r8, 0
+    mov     r9, 0
+    mov     rax, 9
+    syscall
+    mov     rcx, rax
+
+    pop     r8  ; length in r8
+    dec     r8  ; this is now the index of the null terminator
+    pop     rsi
+    pop     rax ; put the number in rax for the div instruction
+
+    cmp     rcx, -1
+    je      .ltos_map_fail
+
+    mov     byte [rcx+r8], 0
+    mov     byte [rcx], "-"
+    lea     r11, [digits] ; load the address of digits into r11 so that we can get some apple pie (get it haha funny)
+    mov     r9, r8
+.L4:
+    dec     r9
+    xor     edx, edx
+    div     rsi ; divide by the base
+    ; remainder in rdx
+    mov     dl, byte [r11+rdx]
+    ; dl is now the character to use
+    mov     byte [rcx+r9], dl
+    test    rax, rax
+    jnz     .L4
+.ltos_done:
+    mov     rax, rcx
+    ret
+.ltos_map_fail:
+    xor     eax, eax
     ret
--- src/tests/ltos.c
+++ src/tests/ltos.c
@@ -1,9 +1,51 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <sys/mman.h>
 
 extern char* ultos(uint64_t, uint64_t);
+extern char* ltos(uint64_t, uint64_t);
+extern uint64_t strlen(const char*);
 
 int main() {
-    printf("%s\n", ultos(64, 10));
+    char* s;
+    s = ultos(64, 10);
+    printf("%s\n", s);
+    munmap(s, strlen(s)+1);
+
+    s = ultos(0, 10);
+    printf("%s\n", s);
+    munmap(s, strlen(s)+1);
+
+    s = ultos(-1, 16);
+    printf("%s\n", s);
+    munmap(s, strlen(s)+1);
+
+    s = ltos(64, 10);
+    printf("%s\n", s);
+    munmap(s, strlen(s)+1);
+
+    s = ltos(1, 16);
+    printf("%s\n", s);
+    munmap(s, strlen(s)+1);
+
+    s = ltos(-64, 10);
+    printf("%s\n", s);
+    munmap(s, strlen(s)+1);
+
+    s = ltos(0, 10);
+    printf("%s\n", s);
+    munmap(s, strlen(s)+1);
+
+    s = ltos(-1, 16);
+    printf("%s\n", s);
+    munmap(s, strlen(s)+1);
+
+    s = ltos(INT64_MIN, 16);
+    printf("%s\n", s);
+    munmap(s, strlen(s)+1);
+
+    s = ltos(INT64_MIN, 10);
+    printf("%s\n", s);
+    munmap(s, strlen(s)+1);
     return 0;
 }
